ab1a01d8883df650603fc8e071d00cda760e28f7
Pedro Henrique <pedrohbc96@gmail.com>
2022-11-03 22:49:33 -0300

    feat: Adiciona e implementa Bean Validation

--- src/main/java/com/phbc/phlogapi/controller/ClienteController.java
+++ src/main/java/com/phbc/phlogapi/controller/ClienteController.java
@@ -3,12 +3,11 @@ package com.phbc.phlogapi.controller;
 import com.phbc.phlogapi.model.Cliente;
 import com.phbc.phlogapi.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
-import org.aspectj.weaver.patterns.ReferencePointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,12 +35,12 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente adicionar(@RequestBody Cliente cliente) {
+    public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
         return clienteRepository.save(cliente);
     }
 
     @PutMapping("/{clienteId}")
-    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,@Valid @RequestBody Cliente cliente) {
         if (!clienteRepository.existsById(clienteId)) {
             return ResponseEntity.notFound().build();
         }
--- src/main/java/com/phbc/phlogapi/model/Cliente.java
+++ src/main/java/com/phbc/phlogapi/model/Cliente.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
@@ -20,7 +23,16 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
+
+    @NotBlank
+    @Size(max = 255)
+    @Email
     private String email;
 }
